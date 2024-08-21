657ed5ee3699f0da06aeec957c56bcf6d7f62cf1
Arjun Biju <arjunbiju332@gmail.com>
2024-08-21 21:38:47 +0530

    completed constituency page

--- MAdmin/views.py
+++ MAdmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from MAdmin.models import *
 
 def admin_home(request):
@@ -7,7 +7,13 @@ def admin_home(request):
 def add_cadmins(request):
     return render(request,'add_admins.html')
 
-def constituency(request):
+def constituency(r):
+    if r.POST:
+        Constituency.objects.create(
+            state=r.POST.get('state'),
+            name=r.POST.get('con')
+            )
+        return redirect('constituency')
     con=Constituency.objects.all()
     context={'con':con}
-    return render(request,'constituency.html',context)
+    return render(r,'constituency.html',context)
